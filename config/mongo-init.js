6762149b67c78f5420a08985b472583b97c1f367
gregdenay <gregoire.denay@cvua-rrw.de>
2024-11-08 14:13:37 +0100

    mongo and mongo-express working and talking to each other

--- config/mongo-init.js
+++ config/mongo-init.js
@@ -1,7 +1,10 @@
+print("Starting mongo-init");
+
 // Create root user
 db.getSiblingDB('admin').auth(process.env.MONGO_INITDB_ROOT_USERNAME, process.env.MONGO_INITDB_ROOT_PASSWORD)
 
 // Create API user
+print("Creating default user");
 db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE)
 db.createUser({
     user: process.env.MONGODB_USERNAME,
@@ -15,14 +18,17 @@ db.createUser({
   });
 
 //   Setting up collections and indexing
+print("Indexing database");
 db.createCollection("isolates")
-db.isolates.createIndex({"isolate_id"})
+db.isolates.createIndex({"isolate_id": 1})
 
 db.createCollection("clusters")
-db.isolates.createIndex({"cluster_id"})
+db.isolates.createIndex({"cluster_id": 1})
 
 db.createCollection("runs")
-db.isolates.createIndex({"run_metadata.run_name"})
+db.isolates.createIndex({"run_metadata.run_name": 1})
 
 db.createCollection("sequences")
-db.isolates.createIndex({"isolate_id"})
+db.isolates.createIndex({"isolate_id": 1})
+
+print("mongo-init done");
